f458d4e886ded15b47e53fa6eee1de005fa09584
Modestas <modestask97@gmail.com>
2018-03-23 19:21:57 +0000

    added jquery scrolling effect when clicking on nav element - takes you to the correct section on the page

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
-console.log('JS loaded');
-console.log('wally');
+$(document).ready(function(){
+  setBindings();
+});
+
+function setBindings() {
+  $('nav a').click(function(e){
+    e.preventDefault();
+    var sectionId = e.currentTarget.id + 'Section';
+
+    $('html,body').animate({
+      scrollTop: $('#' + sectionId).offset().top
+    }, 1000);
+  });
+}
