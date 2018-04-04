08ae23e6a1ffc1b79804ef774a283cfff0c4b635
Modestas <modestask97@gmail.com>
2018-04-04 18:12:17 +0100

    added timeout to tooltip

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
   setBindings();
 
+  // shows tooltip on pageload
   $(function () {
     $('[data-toggle="tooltip"]').tooltip('show');
   });
 
+  // fades out tooltip after 4 secs
+  setTimeout(function() {
+    $('.tooltip').fadeOut('slow');
+  }, 3000);
+
+
+  // tooltip fades in when you hover over nav item
+  $('[data-toggle="tooltip"]').hover(function() {
+    $('.tooltip').fadeIn( 500 );
+  });
 
 });
 
