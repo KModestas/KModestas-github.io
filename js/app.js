8e320c0ecfaaa2a5438a07ec694ce927c0cbcf43
Modestas <modestask97@gmail.com>
2019-12-11 16:41:33 +0000

    navbar no longer getting in the way of scroll

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function() {
   setBindings();
 
   // shows tooltip on pageload
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip('show');
+  $(function() {
+    $('[data-toggle="tooltip"]').tooltip("show");
   });
 
   // fades out tooltip after 4 secs
   setTimeout(function() {
-    $('.tooltip').fadeOut('slow');
+    $(".tooltip").fadeOut("slow");
   }, 4000);
-
 
   // tooltip fades in when you hover over nav item
   $('[data-toggle="tooltip"]').hover(function() {
-    $('.tooltip').fadeIn( 500 );
+    $(".tooltip").fadeIn(500);
   });
-
 });
 
 function setBindings() {
-  $('nav a').click(function(e){
+  $("nav a").click(function(e) {
     e.preventDefault();
-    const sectionId = e.currentTarget.id + 'Section';
+    const sectionId = e.currentTarget.id + "Section";
 
-    $('html,body').animate({
-      scrollTop: $('#' + sectionId).offset().top
-    }, 1500);
+    $("html,body").animate(
+      {
+        // - 50px to overcome navbar height
+        scrollTop: $("#" + sectionId).offset().top - 50
+      },
+      1500
+    );
   });
-
-
 }
